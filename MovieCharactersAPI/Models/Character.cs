834902b1596445e8b505f5893511556c7bf66ef3
Marius Samson <mariusbsamson@gmail.com>
2022-02-28 15:10:12 +0100

    INIT: Project skeleton with Character model

---
+++ MovieCharactersAPI/Models/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieCharactersAPI.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public string Gender { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ MovieCharactersAPI/Models/CharacterDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieCharactersAPI.Models
+{
+    public class CharacterDbContext : DbContext
+    {
+        public DbSet<Character> Character { get; set; }
+
+        public CharacterDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
